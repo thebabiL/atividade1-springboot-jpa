3230cdd970bcfc764a6bd43562595376fc20f819
Bárbara Leidemer <barbara.leidemer.pb@compasso.com.br>
2025-01-14 17:34:40 -0300

    Banco de dados H2, teste e JPA

--- src/main/java/com/entities/Usuario.java
+++ src/main/java/com/entities/Usuario.java
@@ -2,9 +2,20 @@ package com.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class Usuario implements Serializable
 {
   private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String nome;
   private String email;
