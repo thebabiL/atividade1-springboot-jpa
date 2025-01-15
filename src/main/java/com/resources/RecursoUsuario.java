d27b76c56f9173c81437dcdb0cb6a329754bdaf1
Bárbara Leidemer <barbara.leidemer.pb@compasso.com.br>
2025-01-15 16:26:47 -0300

    Camada de serviço, registro de componente

--- src/main/java/com/resources/RecursoUsuario.java
+++ src/main/java/com/resources/RecursoUsuario.java
@@ -1,20 +1,28 @@
 package com.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.entities.Usuario;
+import com.services.ServicoUsuario;
 
 @RestController
 @RequestMapping(value = "/usuarios")
 public class RecursoUsuario 
 {
+   @Autowired
+   private ServicoUsuario servico;
+
   @GetMapping
-   public ResponseEntity<Usuario> encontrarTodos()
+   public ResponseEntity<List<Usuario>> findAll()
    {
-      Usuario u = new Usuario(1L, "Maria", "maria@gmail.com", "9999999", "12345");
-      return ResponseEntity.ok().body(u);
+      List<Usuario> lista = servico.findAll();
+      
+      return ResponseEntity.ok().body(lista);
    }
 }
---
+++ src/main/java/com/services/ServicoUsuario.java
@@ -0,0 +1,29 @@
+package com.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.entities.Usuario;
+import com.repositories.RepositorioUsuario;
+
+@Service
+public class ServicoUsuario 
+{
+  @Autowired
+  private RepositorioUsuario repositorio;
+
+  public List<Usuario> findAll()
+  {
+    return repositorio.findAll();
+  }
+
+  public Usuario findById(Long id)
+  {
+    Optional<Usuario> obj = repositorio.findById(id);
+
+    return obj.get();
+  }
+}
